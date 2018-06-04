c0f91c686f19c8ce3ff2c88127163df95e228312
thttnt <848815969@qq.com>
2018-06-04 23:21:21 +0800

    优化代码格式

--- buscom.py
+++ buscom.py
@@ -1,13 +1,14 @@
 import wiringpi2 as wpi
 
+
 def init():
-    print ("I2C is starting...")
-    print ("I2C is ready!")
+    print("I2C is starting...")
+    print("I2C is ready!")
 
 
-def writeData(address,str):
+def writeData(address, str):
     str = str + "*"
     bs = str.encode()
     bus = wpi.wiringPiI2CSetup(address)
     for byte in bs:
-        wpi.wiringPiI2CWrite(bus,int(byte))
+        wpi.wiringPiI2CWrite(bus, int(byte))
--- facetrack.py
+++ facetrack.py
@@ -4,45 +4,44 @@ import cv2
 import os
 import threading
 
+
 def init(buscom):
-   print("Facetrack is starting...")
-   global bc
-   global camera
-   global rawCapture
-   global face_cascade
-   bc = buscom
-   camera = PiCamera()
-   camera.resolution = (320,240)
-   camera.framerate = 60
-   camera.brightness = 70
-   rawCapture = PiRGBArray(camera, size = (320,240))
-   rawCapture.seek(0)
-   face_cascade = cv2.CascadeClassifier('/home/pi/develop/opencv-3.4.1/data/lbpcascades/lbpcascade_frontalface.xml')
-   threading.Thread(target=track).start()
-   print("Facetrack is ready!")  
+    print("Facetrack is starting...")
+    global bc
+    global camera
+    global rawCapture
+    global face_cascade
+    bc = buscom
+    camera = PiCamera()
+    camera.resolution = (320, 240)
+    camera.framerate = 60
+    camera.brightness = 70
+    rawCapture = PiRGBArray(camera, size=(320, 240))
+    rawCapture.seek(0)
+    face_cascade = cv2.CascadeClassifier('/home/pi/develop/opencv-3.4.1/data/lbpcascades/lbpcascade_frontalface.xml')
+    threading.Thread(target=track).start()
+    print("Facetrack is ready!")
 
 
 def track():
-   for frame in camera.capture_continuous(rawCapture,format="bgr",use_video_port=True):
-       image = frame.array
-       gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-       faces = face_cascade.detectMultiScale(gray)
-       max_w = 0
-       max_h = 0
-       max_x = 0
-       max_y = 0
-       size = 0
-       for (x,y,w,h) in faces:
-           if (w * h > max_w * max_h):
-               max_w = w
-               max_h = h
-               max_x = x
-               max_y = y
-           size = size +1
-           if size > 0:
-               bc.writeData(0x04,"001|"+str(size)+"|"+str(max_x)+"|"+str(max_y)+"|"+str(max_w)+"|"+str(max_h))
-       rawCapture.seek(0)
-       
-       
-       
-       
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray)
+        max_w = 0
+        max_h = 0
+        max_x = 0
+        max_y = 0
+        size = 0
+        for (x, y, w, h) in faces:
+            if w * h > max_w * max_h:
+                max_w = w
+                max_h = h
+                max_x = x
+                max_y = y
+            size = size + 1
+            if size > 0:
+                bc.writeData(0x04,
+                             "001|" + str(size) + "|" + str(max_x) + "|" + str(max_y) + "|" + str(max_w) + "|" + str(
+                                 max_h))
+        rawCapture.seek(0)
